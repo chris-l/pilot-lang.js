e158706c3e5884257bb8c5aaacdaf77161c100e7
Christopher Luna <dev@christopher-luna.com>
2016-11-30 01:12:26 -0600

    Put text preparation on it's own module

--- instructions/type.js
+++ instructions/type.js
@@ -1,23 +1,10 @@
 /*jslint node:true,indent:2*/
 'use strict';
+var prepareText = require('../lib/prepareText');
 
 module.exports = function (instruction) {
   var text, self = this;
 
-  text = instruction.text.reduce(function (str, part) {
-    if (typeof part === 'string') {
-      return str + part;
-    }
-
-    switch (part.element) {
-    case 'string_ident':
-      return str + self.identifiers.strings[part.value] || '';
-    case 'numeric_ident':
-      return str + self.identifiers.numeric[part.value] || '';
-    case 'internal_ident':
-      return str + self[part.value] || '';
-    }
-  }, '');
-
+  text = prepareText(self, instruction.text);
   self.output(text);
 };
