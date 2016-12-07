08e9fe2bf0bd9c057134d409e9987557e8191358
Christopher Luna <dev@christopher-luna.com>
2016-12-06 20:06:57 -0600

    Added browser based demo

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ program: lint parser dist
 parser:
 	node_modules/pegjs/bin/pegjs -o grammar/parser.js grammar/pilot.pegjs
 lint:
-	node_modules/jslint/bin/jslint.js --indent 2 --color package.json main.js demo/node-interpreter.js instructions/*.js lib/*.js
+	node_modules/jslint/bin/jslint.js --indent 2 --color package.json main.js demo/*.js instructions/*.js lib/*.js
 dist: parser
 	node_modules/browserify/bin/cmd.js -s pilotCreator main.js | node_modules/uglifyjs/bin/uglifyjs > dist/pilot.min.js
---
+++ demo/demo.js
@@ -0,0 +1,76 @@
+/*jslint browser:true, indent:2*/
+/*global pilotCreator*/
+(function (global) {
+  'use strict';
+  var area, pilot, buffer = '', sent = false;
+
+  area = document.querySelector('#output');
+  area.addEventListener('keydown', function (e) {
+
+    // Prevent backspace if the buffer is empty
+    if (e.which === 8) {
+      if (buffer.length > 0) {
+        buffer = buffer.substr(0, buffer.length - 1);
+      } else {
+        e.preventDefault();
+      }
+    }
+
+    // Ignore the arrow keys
+    if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+    }
+  });
+
+  area.addEventListener('keypress', function (e) {
+    // Pressing enter sets "sent" as true
+    if (e.which === 13) {
+      sent = true;
+      return;
+    }
+
+    // Append the char to the buffer
+    buffer += String.fromCharCode(e.which);
+  }, false);
+
+  // Print text on the textarea
+  function print(x) {
+    area.value += x;
+    area.scrollTop = area.scrollHeight;
+    area.focus();
+  }
+
+  // Read the input
+  function input(fn) {
+    var inter;
+
+    sent = false;
+    buffer = '';
+    print(':');
+    inter = setInterval(function () {
+      if (sent === true) {
+        sent = false;
+        fn(buffer);
+        buffer = '';
+        clearInterval(inter);
+      }
+    }, 0);
+  }
+
+  // Create the PILOT instance
+  pilot = pilotCreator(input, print);
+
+  // Listener for the "Run" button
+  document.querySelector('#run').addEventListener('click', function () {
+    try {
+      pilot.execute(document.querySelector('#code').value);
+    } catch (e) {
+      print(e);
+      print('\n');
+    }
+  });
+
+  // Yes, set the pilot object as global
+  global.pilot = pilot;
+
+}(this));
